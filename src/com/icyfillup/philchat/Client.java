88c52443c4da48e416d9b06634e350f05bf44fb9
icyfillup <phil10tran@gmail.com>
2014-07-28 00:04:23 -0700

    the text input can now input messages on the text board from enter key or the send button

--- src/com/icyfillup/philchat/Client.java
+++ src/com/icyfillup/philchat/Client.java
@@ -5,11 +5,22 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
+
 import java.awt.GridBagLayout;
+
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
+
 import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Client extends JFrame
 {
@@ -19,7 +30,9 @@ public class Client extends JFrame
 	
 	private String name, address;
 	private int port;
-
+	private JTextField txtMessage;
+	private JTextArea txtrHistory;
+	
 	public Client(String name, String address, int port)
 	{
 		setTitle("Phil Chat Client");
@@ -28,10 +41,20 @@ public class Client extends JFrame
 		this.port = port;
 		
 		createWindow();
+		console("Attempting a connection to " + address + ", " + port + ". User: " + name);
 	}
 	
 	private void createWindow()
 	{
+		try
+		{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(880, 550);
 		setLocationRelativeTo(null);
@@ -40,19 +63,70 @@ public class Client extends JFrame
 		setContentPane(contentPane);
 		
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{29, 847, 4};
-		gbl_contentPane.rowHeights = new int[]{45, 475, 30};
-		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[]{28, 815, 30, 7};
+		gbl_contentPane.rowHeights = new int[]{35, 475, 40};
+		gbl_contentPane.columnWeights = new double[]{1.0, 1.0};
 		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
-		JTextArea txtrHistory = new JTextArea();
+		txtrHistory = new JTextArea();
+		txtrHistory.setEditable(false);
 		GridBagConstraints gbc_txtrHistory = new GridBagConstraints();
+		gbc_txtrHistory.insets = new Insets(0, 0, 5, 5);
 		gbc_txtrHistory.fill = GridBagConstraints.BOTH;
 		gbc_txtrHistory.gridx = 1;
 		gbc_txtrHistory.gridy = 1;
+		gbc_txtrHistory.gridwidth = 2;
+		gbc_txtrHistory.insets = new Insets(0, 5, 0, 0);
 		contentPane.add(txtrHistory, gbc_txtrHistory);
 		
+		txtMessage = new JTextField();
+		txtMessage.addKeyListener(new KeyAdapter()
+		{
+			public void keyPressed(KeyEvent e)
+			{
+				if(e.getKeyCode() == KeyEvent.VK_ENTER)
+				{
+					sent(txtMessage.getText());
+				}
+			}
+		});
+		GridBagConstraints gbc_txtMessage = new GridBagConstraints();
+		gbc_txtMessage.insets = new Insets(0, 0, 0, 5);
+		gbc_txtMessage.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMessage.gridx = 1;
+		gbc_txtMessage.gridy = 2;
+		contentPane.add(txtMessage, gbc_txtMessage);
+		txtMessage.setColumns(10);
+		
+		JButton btnSent = new JButton("Sent");
+		btnSent.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				sent(txtMessage.getText());
+			}
+		});
+		GridBagConstraints gbc_btnSent = new GridBagConstraints();
+		gbc_btnSent.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSent.gridx = 2;
+		gbc_btnSent.gridy = 2;
+		contentPane.add(btnSent, gbc_btnSent);
+		
 		setVisible(true);
+		txtMessage.requestFocusInWindow();
+	}
+	
+	private void sent(String message)
+	{
+		if(message.equals("")) return;
+		message = name + ": " + message;
+		console(message);
+		txtMessage.setText("");
+	}
+	
+	public void console(String message)
+	{
+		txtrHistory.append(message + "\n\r");
 	}
 }
